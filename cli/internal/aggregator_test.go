6906af65d2c3eb5338eca0367acd41ff316161da
datkach <dmitriy.tkach03@gmail.com>
2024-06-13 00:06:07 +0300

    Update tests for aggregator.

--- cli/internal/aggregator_test.go
+++ cli/internal/aggregator_test.go
@@ -3,201 +3,287 @@ package internal
 import (
 	"NewsAggregator/cli/internal/entity"
 	"NewsAggregator/cli/internal/filters"
-	"os"
-	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
 )
 
-func setWorkingDirectory(t *testing.T) func() {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("failed to get working directory: %v", err)
-	}
-	rootPath := filepath.Join(wd, "../..")
-	err = os.Chdir(rootPath)
-	if err != nil {
-		t.Fatalf("failed to change working directory: %v", err)
-	}
-	return func() {
-		err := os.Chdir(wd)
-		if err != nil {
-			t.Fatalf("failed to restore working directory: %v", err)
-		}
-	}
-}
-
 func TestAggregate(t *testing.T) {
-	restoreWD := setWorkingDirectory(t)
-	defer restoreWD()
 	type args struct {
-		sources []string
-		filters []NewsFilter
+		Resources   []entity.Resource
+		Sources     []string
+		NewsFilters []NewsFilter
 	}
 	tests := []struct {
 		name string
 		args args
 		want []entity.News
 	}{{
-		name: "should aggregate news on given sources applying filters.",
+		name: "should aggregate news on given Sources applying NewsFilters.",
 		args: args{
-			sources: []string{"BBC"},
-			filters: []NewsFilter{
-				&filters.Keyword{Keywords: []string{"Taiwan", "Israel"}},
+			Resources: []entity.Resource{
+				{
+					"RSS", "testdata/news.xml",
+				},
+				{
+					"HTML", "testdata/news.html",
+				},
+				{
+					"JSON", "testdata/news.json",
+				},
+			},
+			Sources: []string{"RSS"},
+			NewsFilters: []NewsFilter{
+				&filters.Keyword{Keywords: []string{"Dymond"}},
 				&filters.DateStart{StartDate: time.Date(2024, time.May, 17, 10, 10, 10, 0, time.UTC)},
 				&filters.DateEnd{EndDate: time.Date(2024, time.May, 18, 23, 30, 10, 0, time.UTC)},
 			},
 		},
 		want: []entity.News{
 			{
-				Title:       "Israel war cabinet minister vows to quit if there is no post-war plan for Gaza",
-				Description: "Recent weeks have seen an increasingly public rift over how Gaza should be governed after the war.",
-				Link:        "https://www.bbc.com/news/articles/cekkz82gnzgo",
-				Date:        time.Date(2024, time.May, 18, 23, 22, 26, 0, time.UTC),
-				Source:      "BBC",
-			},
-			{
-				Title:       "Taiwan's steely leader rewrote the book on how to deal with China",
-				Description: "Taiwan's outgoing president says boosting her country's military was the only way to defy China's threat.",
-				Link:        "https://www.bbc.com/news/articles/ceklk794102o",
-				Date:        time.Date(2024, time.May, 18, 22, 24, 57, 0, time.UTC),
-				Source:      "BBC",
-			},
-			{
-				Title:       "Stories of the hostages taken by Hamas from Israel",
-				Description: "It is thought more than 100 Israelis are still being held hostage in Gaza after the 7 October attacks.",
-				Link:        "https://www.bbc.co.uk/news/world-middle-east-67053011",
-				Date:        time.Date(2024, time.May, 18, 22, 30, 16, 0, time.UTC),
-				Source:      "BBC",
+				Title:       "Su and Steve fought for justice, but didn't live to see it",
+				Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+				Link:        "https://www.bbc.co.uk/news/health-69018125",
+				Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+				Source:      "RSS",
 			},
 		},
 	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Aggregate(tt.args.sources, tt.args.filters); !reflect.DeepEqual(got, tt.want) {
+			a := Aggregator{
+				Resources:   tt.args.Resources,
+				Sources:     tt.args.Sources,
+				NewsFilters: tt.args.NewsFilters,
+			}
+			if got := a.Aggregate(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Aggregate() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-
-func Test_applyFilters(t *testing.T) {
-	restoreWD := setWorkingDirectory(t)
-	defer restoreWD()
+func TestAggregator_applyFilters(t *testing.T) {
+	type fields struct {
+		NewsFilters []NewsFilter
+	}
 	type args struct {
-		news    []entity.News
-		filters []NewsFilter
+		news []entity.News
 	}
 	tests := []struct {
-		name string
-		args args
-		want []entity.News
+		name   string
+		fields fields
+		args   args
+		want   []entity.News
 	}{{
-		name: "should apply filters to selected news.",
+		name: "should aggregate news on given Sources applying NewsFilters.",
+		fields: fields{
+			NewsFilters: []NewsFilter{
+				&filters.Keyword{Keywords: []string{"Dymond"}},
+				&filters.DateStart{StartDate: time.Date(2024, time.May, 17, 10, 10, 10, 0, time.UTC)},
+				&filters.DateEnd{EndDate: time.Date(2024, time.May, 18, 23, 30, 10, 0, time.UTC)},
+			},
+		},
 		args: args{
-			news: collectNews([]string{"NBC"}),
-			filters: []NewsFilter{
-				&filters.Keyword{Keywords: []string{"Taiwan"}},
+			news: []entity.News{
+				{
+					Title:       "Boy's body found in river as second teen 'critical'",
+					Description: "The 14-year-old's body was found in the River Tyne and the younger boy was rescued and taken to hospital.",
+					Link:        "https://www.bbc.com/news/articles/cnee7lp7mgdo",
+					Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+					Source:      "RSS",
+				},
+				{
+					Title:       "Su and Steve fought for justice, but didn't live to see it",
+					Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+					Link:        "https://www.bbc.co.uk/news/health-69018125",
+					Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+					Source:      "RSS",
+				},
 			},
 		},
 		want: []entity.News{
 			{
-				Title:       "Taiwan lawmakers exchange blows in bitter dispute over parliament reforms",
-				Description: "Lawmakers in Taiwan shoved, tackled and hit each other in a bitter dispute about parliamentary reforms, days before President-elect Lai Ching-te takes office/",
-				Link:        "https://www.nbcnews.com/news/world/taiwan-lawmakers-exchange-blows-bitter-dispute-parliament-reforms-rcna152763",
-				Date:        time.Date(2024, time.May, 17, 15, 42, 56, 0, time.UTC),
-				Source:      "NBC",
+				Title:       "Su and Steve fought for justice, but didn't live to see it",
+				Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+				Link:        "https://www.bbc.co.uk/news/health-69018125",
+				Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+				Source:      "RSS",
 			},
 		},
 	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := applyFilters(tt.args.news, tt.args.filters); !reflect.DeepEqual(got, tt.want) {
+			a := Aggregator{
+				NewsFilters: tt.fields.NewsFilters,
+			}
+			if got := a.applyFilters(tt.args.news); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("applyFilters() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_collectNews(t *testing.T) {
-	restoreWD := setWorkingDirectory(t)
-	defer restoreWD()
+func TestAggregator_collectNews(t *testing.T) {
+	type fields struct {
+		Resources []entity.Resource
+	}
 	type args struct {
 		sources []string
 	}
 	tests := []struct {
-		name string
-		args args
-		want int
-	}{{
-		name: "should collect news from the specified sources.",
-		args: args{
-			sources: []string{"usatoday"},
+		name   string
+		fields fields
+		args   args
+		want   []entity.News
+	}{
+		{
+			name: "should aggregate news on given Sources applying NewsFilters.",
+			fields: fields{
+				Resources: []entity.Resource{
+					{
+						"RSS", "testdata/news.xml",
+					},
+					{
+						"HTML", "testdata/news.html",
+					},
+					{
+						"JSON", "testdata/news.json",
+					},
+				},
+			},
+			args: args{
+				sources: []string{"RSS"},
+			},
+			want: []entity.News{
+
+				{
+					Title:       "Boy's body found in river as second teen 'critical'",
+					Description: "The 14-year-old's body was found in the River Tyne and the younger boy was rescued and taken to hospital.",
+					Link:        "https://www.bbc.com/news/articles/cnee7lp7mgdo",
+					Date:        time.Date(2024, time.May, 19, 12, 20, 49, 0, time.UTC),
+					Source:      "RSS",
+				},
+				{
+					Title:       "Su and Steve fought for justice, but didn't live to see it",
+					Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+					Link:        "https://www.bbc.co.uk/news/health-69018125",
+					Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+					Source:      "RSS",
+				},
+			},
 		},
-		want: 33,
-	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := collectNews(tt.args.sources); !reflect.DeepEqual(len(got), tt.want) {
-				t.Errorf("collectNews() = %v, want %v", len(got), tt.want)
+			a := Aggregator{
+				Resources: tt.fields.Resources,
+			}
+			if got := a.collectNews(tt.args.sources); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("collectNews() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_getForSource(t *testing.T) {
-	restoreWD := setWorkingDirectory(t)
-	defer restoreWD()
+func TestAggregator_getNewsForSource(t *testing.T) {
+	type fields struct {
+		Resources []entity.Resource
+	}
 	type args struct {
 		sourceName string
 	}
 	tests := []struct {
-		name string
-		args args
-		want int
-	}{{
-		name: "should collect news from the specified source.",
-		args: args{
-			sourceName: "abc",
+		name   string
+		fields fields
+		args   args
+		want   []entity.News
+	}{
+		{
+			name: "should aggregate news on given Sources applying NewsFilters.",
+			fields: fields{
+				Resources: []entity.Resource{
+					{
+						"RSS", "testdata/news.xml",
+					},
+					{
+						"HTML", "testdata/news.html",
+					},
+					{
+						"JSON", "testdata/news.json",
+					},
+				},
+			},
+			args: args{
+				"RSS",
+			},
+			want: []entity.News{
+				{
+					Title:       "Boy's body found in river as second teen 'critical'",
+					Description: "The 14-year-old's body was found in the River Tyne and the younger boy was rescued and taken to hospital.",
+					Link:        "https://www.bbc.com/news/articles/cnee7lp7mgdo",
+					Date:        time.Date(2024, time.May, 19, 12, 20, 49, 0, time.UTC),
+					Source:      "RSS",
+				},
+				{
+					Title:       "Su and Steve fought for justice, but didn't live to see it",
+					Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+					Link:        "https://www.bbc.co.uk/news/health-69018125",
+					Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+					Source:      "RSS",
+				},
+			},
 		},
-		want: 25,
-	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getForSource(tt.args.sourceName); !reflect.DeepEqual(len(got), tt.want) {
-				t.Errorf("getForSource() = %v, want %v", len(got), tt.want)
+			a := Aggregator{
+				Resources: tt.fields.Resources,
+			}
+			if got := a.getNewsForSource(tt.args.sourceName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getNewsForSource() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_getResourceNews(t *testing.T) {
-	restoreWD := setWorkingDirectory(t)
-	defer restoreWD()
+func TestAggregator_getResourceNews(t *testing.T) {
+
 	type args struct {
-		resource entity.Resource
+		path entity.PathToFile
 	}
 	tests := []struct {
 		name string
 		args args
-		want int
-	}{{
-		name: "should parses news from a single resource",
-		args: args{
-			resource: entity.Resource{Name: "washington", PathToFile: "resources/washingtontimes-world-category-19-05-24.xml"},
+		want []entity.News
+	}{
+		{
+			name: "should aggregate news on given Sources applying NewsFilters.",
+			args: args{
+				"testdata/news.xml",
+			},
+			want: []entity.News{
+				{
+					Title:       "Boy's body found in river as second teen 'critical'",
+					Description: "The 14-year-old's body was found in the River Tyne and the younger boy was rescued and taken to hospital.",
+					Link:        "https://www.bbc.com/news/articles/cnee7lp7mgdo",
+					Date:        time.Date(2024, time.May, 19, 12, 20, 49, 0, time.UTC),
+				},
+				{
+					Title:       "Su and Steve fought for justice, but didn't live to see it",
+					Description: "Su Gorman and Steve Dymond helped to expose an NHS scandal - but they died without seeing justice.",
+					Link:        "https://www.bbc.co.uk/news/health-69018125",
+					Date:        time.Date(2024, time.May, 18, 23, 05, 19, 0, time.UTC),
+				},
+			},
 		},
-		want: 20,
-	},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getResourceNews(tt.args.resource); !reflect.DeepEqual(len(got), tt.want) {
-				t.Errorf("getResourceNews() = %v, want %v", len(got), tt.want)
+			a := &Aggregator{}
+			if got := a.getResourceNews(tt.args.path); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getResourceNews() = %v, want %v", got, tt.want)
 			}
 		})
 	}
