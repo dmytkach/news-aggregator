1c7b284b9a8a1a815aa67e44b4e92ccae63a491a
datkach <dmitriy.tkach03@gmail.com>
2024-06-18 09:57:42 +0300

    Add stemming algorithm;

--- cli/internal/filters/keyword.go
+++ cli/internal/filters/keyword.go
@@ -1,7 +1,9 @@
 package filters
 
 import (
+	"github.com/reiver/go-porterstemmer"
 	"news-aggregator/internal/entity"
+	"slices"
 	"strings"
 )
 
@@ -13,16 +15,27 @@ type Keyword struct {
 // Filter news by keywords in the title and description.
 func (k *Keyword) Filter(news []entity.News) []entity.News {
 	var filtered []entity.News
+	keywords := getStemKeywords(k)
 	for _, item := range news {
-		for _, keyword := range k.Keywords {
-			if strings.Contains(strings.ToLower(string(item.Title)), strings.ToLower(keyword)) ||
-				strings.Contains(strings.ToLower(string(item.Description)), strings.ToLower(keyword)) {
+		titles := strings.Split(strings.ToLower(string(item.Title)), " ")
+		description := strings.Split(strings.ToLower(string(item.Description)), " ")
+		for _, stemmedKeyword := range keywords {
+			if slices.Contains(titles, stemmedKeyword) || slices.Contains(description, stemmedKeyword) {
 				filtered = append(filtered, item)
 				break
 			}
 		}
 	}
 	return filtered
+}
+
+func getStemKeywords(k *Keyword) []string {
+	var stemmedWords = make([]string, 0)
+	for _, keyword := range k.Keywords {
+		stemmedWords = append(stemmedWords, strings.ToLower(keyword))          // add original keyword
+		stemmedWords = append(stemmedWords, porterstemmer.StemString(keyword)) // add stemmed keyword
+	}
+	return stemmedWords
 }
 func (k *Keyword) String() string {
 	return "keywords=" + strings.Join(k.Keywords, ",")
