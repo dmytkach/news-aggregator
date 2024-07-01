eabec6415e35a65258bdb3af8956900e77dc3201
dmytkach <dmytro.tkach@teamdev.com>
2024-07-01 08:07:35 +0300

    Update docs for initializers.

---
+++ cli/internal/initializers/doc.go
@@ -0,0 +1,3 @@
+// Package initializers provide utility functions for initializing
+// necessary resources and filters required for news aggregation.
+package initializers
--- cli/internal/initializers/resource_init.go
+++ cli/internal/initializers/resource_init.go
@@ -6,6 +6,9 @@ import (
 	"path/filepath"
 )
 
+// LoadStaticResourcesFromFolder walks through a directory and loads static resources.
+// It returns a map where keys are resource names
+// and values are slices of file paths for each resource.
 func LoadStaticResourcesFromFolder(resourceFolder string) (map[string][]string, error) {
 	newsMap := make(map[string][]string)
 	err := filepath.Walk(resourceFolder, func(path string, info os.FileInfo, err error) error {
