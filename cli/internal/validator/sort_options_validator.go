c46a3419858a3348ef530cd8094919eb83037afa
dmytkach <dmytro.tkach@teamdev.com>
2024-07-09 08:43:47 +0300

    Add docs for sort validator.

--- cli/internal/validator/sort_options_validator.go
+++ cli/internal/validator/sort_options_validator.go
@@ -8,6 +8,9 @@ type sortOptionsValidator struct {
 	order     string
 }
 
+// Validate checks if the provided sorting criterion and order are valid.
+// If a criterion is specified, it must be either "date" or "source".
+// If an order is specified, it must be either "asc" or "desc".
 func (v sortOptionsValidator) Validate() bool {
 	if v.criterion != "" {
 		if !strings.EqualFold(v.criterion, "date") && !strings.EqualFold(v.criterion, "source") {
