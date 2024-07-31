8f684a1fe482ab1c44e25b33e70d8da2a3229487
dmytkach <dmytro.tkach@teamdev.com>
2024-07-31 11:03:27 +0300

    Fix import errors.

---
+++ cronjob/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"news-aggregator/server/managers"
+	"news-aggregator/server/service"
+)
+
+func main() {
+	pathToSourcesFile := flag.String("path_to_source", "../sources.json", "Path to the file containing news sources. Default is 'server/sources.json'.")
+	pathToNews := flag.String("news_folder", "server-news/", "Path to the folder where news files are stored. Default is 'server-news/'.")
+
+	flag.Parse()
+
+	sourceFolder := managers.CreateSourceFolder(*pathToSourcesFile)
+	newsFolder := managers.CreateNewsFolder(*pathToNews)
+	urlFeed := managers.UrlFeed{}
+	fetcher := service.Fetch{
+		SourceManager: sourceFolder,
+		NewsManager:   newsFolder,
+		FeedManager:   urlFeed,
+	}
+
+	err := fetcher.UpdateNews()
+	if err != nil {
+		log.Printf("Error fetching news: %v", err)
+	}
+}
